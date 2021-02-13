c12c50c303b7310e8d2c5a85e62d0b71d1b0a10c
James Cameron <james.cameron@coxautoinc.com>
2021-02-13 15:47:06 -0500

    first

---
+++ src/main/groovy/com/lucidsd/specter/SpecterApplication.groovy
@@ -0,0 +1,13 @@
+package com.lucidsd.specter
+
+import org.springframework.boot.SpringApplication
+import org.springframework.boot.autoconfigure.SpringBootApplication
+
+@SpringBootApplication
+class SpecterApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(SpecterApplication, args)
+	}
+
+}
