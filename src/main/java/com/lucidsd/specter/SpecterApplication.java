8c271e64b31394d44456108764a84eae993f6ce6
James Cameron <james.cameron@coxautoinc.com>
2021-02-13 16:07:26 -0500

    two time forsenCD

---
+++ src/main/java/com/lucidsd/specter/SpecterApplication.java
@@ -0,0 +1,13 @@
+package com.lucidsd.specter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecterApplication.class, args);
+	}
+
+}
