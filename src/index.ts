8831e8652472e6de6f6cb7a37cc3a41284fc5da8
James Cameron <james.cameron@coxautoinc.com>
2021-02-14 15:19:42 -0500

    tsconfig stuff

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,5 @@ const PORT = 3000;
 const HOST = '0.0.0.0';
 app.get('/', (req, res) => res.send('Express + TypeScript Server'));
 app.listen(PORT, HOST);
+// eslint-disable-next-line no-console
 console.log(`Running on http://${HOST}:${PORT}`);
